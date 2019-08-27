0e6bda7715e99ec13ed3cf2084d4439de5970596
Jhonatan Constantino <jhonatan.c16@aluno.ifsc.edu.br>
2019-08-27 08:39:22 -0300

    Adicionando Classe Batedeira

---
+++ src/main/java/POO/Batedeira.java
@@ -0,0 +1,22 @@
+package POO;
+
+public class Batedeira {
+    private boolean ligada;
+    private int velocidade;
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public boolean isLigada() {
+        return ligada;
+    }
+
+    public void setLigada(boolean ligada) {
+        this.ligada = ligada;
+    }
+}
